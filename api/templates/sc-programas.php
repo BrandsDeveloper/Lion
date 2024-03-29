aa0a42198bbc58ed1156a844f41cfa95acff2558
BrandsDev <lucasbrandao392@gmail.com>
2024-03-28 23:42:37 -0300

    tentando o funcionamento de rotas 3

---
+++ api/templates/sc-programas.php
@@ -0,0 +1,17 @@
+<section class="section-custom" id="programas">
+    <div class="border-animate w-100"> </div>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-lg-8">
+                <div class="d-flex flex-column h-100 align-items-start justify-content-center ">
+                    <p class="py-1 px-4 rounded-5 border border-1 border-primary text-primary mb-5"> Programas </p>
+                    <h1 class="fw-light mb-0">Minhas criações se forma <br> <span class="h1 text-primary fw-light">em muitas telas! </span> </h1>
+                    <p class="opacity-50 fw-lighter">Essas são as minhas vantagens</p>
+                </div>
+            </div>
+            <div class="col-lg-auto opacity-50">
+                <?php include 'card.php'; ?>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ api/templates/sc-sobre.php
@@ -0,0 +1,17 @@
+<section class="section-custom" id="sobre">
+    <div class="border-animate w-100"> </div>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-lg-7">
+                <div class="d-flex flex-column h-100 align-items-start justify-content-center ">
+                    <p class="py-1 px-4 rounded-5 border border-1 border-primary text-primary mb-5"> Sobre </p>
+                    <h1 class="text-white fw-light mb-3"> Todo grande projeto começa <br> com <span class="h1 fw-light text-primary">uma história ainda melhor.</span> </h1>
+                    <p class="opacity-25 fw-lighter "> Desde o início da minha jornada como designer independente há quase 8 anos, tenho realizado trabalhos à distância para agências, prestado consultoria para startups e colaborado com indivíduos talentosos para desenvolver produtos digitais para uso empresarial e do consumidor. Sou confiante de maneira discreta, naturalmente curioso e constantemente em busca de aprimorar minhas habilidades, enfrentando um problema de design por vez. </p>
+                </div>
+            </div>
+            <div class="col-lg-auto opacity-50">
+                <?php include 'card.php'; ?>
+            </div>
+        </div>
+    </div>
+</section>
