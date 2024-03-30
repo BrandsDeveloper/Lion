362e355797e0efc8b641d2665d8ba3508fb61efa
BrandsDev <lucasbrandao392@gmail.com>
2024-03-30 15:35:44 -0300

    ajuste 4

--- api/conn.php
+++ api/conn.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = getenv('POSTGRES_HOST');
-$db = getenv('POSTGRES_DATABASE');
-$user = getenv('POSTGRES_USER');
-$pass = getenv('POSTGRES_PASSWORD');
+// $host = getenv('POSTGRES_HOST');
+// $db = getenv('POSTGRES_DATABASE');
+// $user = getenv('POSTGRES_USER');
+// $pass = getenv('POSTGRES_PASSWORD');
 
-// $host = getenv('DB_HOST');
-// $db = getenv('DB_TABLE');
-// $user = getenv('DB_USER');
-// $pass = getenv('DB_PASS');
+$host = getenv('DB_HOST');
+$db = getenv('DB_TABLE');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 
 $conn = new mysqli($host, $user, $pass, $db);
 
