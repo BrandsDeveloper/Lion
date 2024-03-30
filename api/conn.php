84798833ea19ce40bade49ff41cd3ae14884f715
BrandsDev <lucasbrandao392@gmail.com>
2024-03-30 15:46:30 -0300

    ajuste 5

--- api/conn.php
+++ api/conn.php
@@ -5,15 +5,36 @@
 // $user = getenv('POSTGRES_USER');
 // $pass = getenv('POSTGRES_PASSWORD');
 
-$host = getenv('DB_HOST');
-$db = getenv('DB_TABLE');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+// $host = $_ENV['DB_HOST'];
+// $db = $_ENV['DB_TABLE'];
+// $user = $_ENV['DB_USER'];
+// $pass = $_ENV['DB_PASS'];
 
-$conn = new mysqli($host, $user, $pass, $db);
+// $conn = new mysqli($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
-    die("Erro de conexão: " . $conn->connect_error);
-} else {
-    echo "Conexão bem-sucedida";
+// if ($conn->connect_error) {
+//     die("Erro de conexão: " . $conn->connect_error);
+// } else {
+//     echo "Conexão bem-sucedida";
+// }
+
+$host =  $_ENV['POSTGRES_HOST'];
+$port = $_ENV['POSTGRES_PORT'];
+$dbname =  $_ENV['POSTGRES_DATABASE'];
+$user =  $_ENV['POSTGRES_USER'];
+$password =  $_ENV['POSTGRES_PASSWORD'];
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
+
+try {
+    // Criar uma conexão PDO
+    $pdo = new PDO($dsn);
+
+    if($pdo){
+        echo "Conectado ao banco de dados PostgreSQL com sucesso!";
+    }
+
+} catch (PDOException $e) {
+    // Se ocorrer um erro na conexão, ele será exibido aqui
+    echo $e->getMessage();
 }
