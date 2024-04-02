914e5870cf16439c791c49773c12f5e34547aeba
BrandsDev <lucasbrandao392@gmail.com>
2024-04-01 22:06:59 -0300

    inserindo ajax teste

--- js/theme.js
+++ js/theme.js
@@ -1,20 +1,15 @@
 
 let url = document.querySelector('head').getAttribute('data-url');
 
-console.log(url);
-
 let contato = document.querySelector('.form-contato');
-console.log(contato);
 let caminho = contato.getAttribute('data-action');
-console.log(caminho);
 let btn = contato.querySelector('button[type="submit"]')
-console.log(btn);
 
 btn.addEventListener('click', () =>{
 
-   let ajax = XMLHttpRequest();
+    let ajax = XMLHttpRequest();
    
-   ajax.open('POST', url+caminho);
-    
+    ajax.open('POST', url+caminho);
+    ajax.send();
     
 });
