a63b760c933f3de886f2e4552570fc1e3ca585ae
BrandsDev <lucasbrandao392@gmail.com>
2024-04-02 22:10:33 -0300

    Ajuste ajax post

--- js/theme.js
+++ js/theme.js
@@ -8,9 +8,26 @@ let btn = contato.querySelector('button[type="button"]')
 btn.addEventListener('click', () =>{
 
     let ajax = new XMLHttpRequest();
+
+    const params = {
+        nome: document.querySelector('#nome').value,
+        email: document.querySelector('#email').value,
+        tel: document.querySelector('#tel').value,
+        valor: document.querySelector('#orcamento').value,
+        projeto: document.querySelector('#projeto').value
+    }
    
     ajax.open('POST', url+caminho);
-    ajax.send();
+    ajax.setRequestHeader('Content-type', 'application/json')
+
+    ajax.onreadystatechange = () =>{
+        if( ajax.readyState == 4 && ajax.status == 200 ){
+            console.log('DEU CERTO!!');
+        } else if( ajax.readyState == 4 && ajax.status == 404 ){
+            console.log('DEU CERTO NO SERVIDOR, MAS DEU ERRO 404 !!');
+        }
+    }
+    ajax.send(JSON.stringify(params)) // Make sure to stringify
     
     console.log(ajax);
 
