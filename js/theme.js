a649a590bbc26aa3003f7c6ba7ff16fb42d10461
BrandsDev <lucasbrandao392@gmail.com>
2024-04-11 23:45:19 -0300

    Testando ajuste 4

--- js/theme.js
+++ js/theme.js
@@ -73,82 +73,93 @@ btn.addEventListener('click', () =>{
 
 });
 
-    // Função de rolagem suave
-    function scrollDown(to, duration) {
-        var to = document.getElementById(to);
-        var to_position = to.getBoundingClientRect().top;
-        var start_position = window.pageYOffset;
-        var distance_to_position = start_position - to_position;
-        var start = null;
-    
-        function step(timestamp) {
-        var progress;
-        if (start === null) start = timestamp;
-        progress = timestamp - start;
-        window.scrollTo(0, smooth(progress, start_position, distance_to_position, duration));
-        if (progress < 2000) {
-            requestAnimationFrame(step);
-        }
-        }
-    
-        function smooth(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
-        }
-    
-        requestAnimationFrame(step);
+let isScrolling = false;
+
+// Função de rolagem suave
+function scrollDown(targetId, duration) {
+  if (isScrolling) return; // Não inicia uma nova rolagem se já estiver rolando
+  isScrolling = true;
+  var to = document.getElementById(targetId);
+  var to_position = to.getBoundingClientRect().top + window.pageYOffset;
+  var start_position = window.pageYOffset;
+  var distance_to_position = to_position - start_position;
+  var start = null;
+
+  function step(timestamp) {
+    var progress;
+    if (start === null) start = timestamp;
+    progress = timestamp - start;
+    window.scrollTo(0, smooth(progress, start_position, distance_to_position, duration));
+    if (progress < duration) {
+      requestAnimationFrame(step);
+    } else {
+      isScrolling = false; // Resseta a flag quando a animação termina
     }
+  }
+
+  function smooth(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(step);
+}
+
+// Captura as seções e suas posições
+const sections = Array.from(document.querySelectorAll('.section-custom')).map(section => {
+  return {
+    id: section.id,
+    offsetTop: section.offsetTop
+  };
+});
+
+// Event listener para links de navegação
+document.querySelectorAll('nav a[data-target]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('data-target');
+    scrollDown(targetId, 2000);
+  });
+});
+
+// Função para tratar o evento de roda do mouse
+function handleWheelEvent(e) {
+  if (isScrolling) {
+    e.preventDefault();
+    return;
+  }
   
-    // Captura as seções e suas posições
-    const sections = Array.from(document.querySelectorAll('.section-custom')).map(section => {
-        return {
-        id: section.id,
-        offsetTop: section.offsetTop
-        };
-    });
-    
-    // Event listener para links de navegação
-    document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('data-target');
-        scrollDown(targetId, 2000);
-        });
-    });
-    
-    // Função para tratar o evento de roda do mouse
-    function handleWheelEvent(e) {
-        // e.preventDefault();
-        const currentPosition = window.pageYOffset;
-        const delta = e.deltaY || e.detail || e.wheelDelta;
-    
-        let targetSection;
-        
-        // Descobre se está rolando para cima ou para baixo e encontra a próxima seção
-        if (delta < 0) { // Rolando para cima
-        for (let i = sections.length - 1; i >= 0; i--) {
-            if (sections[i].offsetTop < currentPosition) {
-            targetSection = sections[i];
-            break;
-            }
-        }
-        } else { // Rolando para baixo
-        for (let i = 0; i < sections.length; i++) {
-            if (sections[i].offsetTop > currentPosition + window.innerHeight) {
-            targetSection = sections[i];
-            break;
-            }
-        }
-        }
-    
-        // Se houver uma seção alvo, rola até ela
-        if (targetSection) {
-        scrollDown(targetSection.id, 2000);
-        }
+  const currentPosition = window.pageYOffset;
+  const delta = e.deltaY || e.detail || e.wheelDelta;
+  let targetSection;
+
+  // Descobre se está rolando para cima ou para baixo e encontra a próxima seção
+  if (delta < 0) { // Rolando para cima
+    for (let i = sections.length - 1; i >= 0; i--) {
+      if (sections[i].offsetTop < currentPosition) {
+        targetSection = sections[i];
+        break;
+      }
     }
-    
-    // Adiciona o event listener para o evento de 'wheel'
-    document.addEventListener('wheel', handleWheelEvent);
+  } else { // Rolando para baixo
+    for (let i = 0; i < sections.length; i++) {
+      if (sections[i].offsetTop > currentPosition + window.innerHeight) {
+        targetSection = sections[i];
+        break;
+      }
+    }
+  }
+
+  // Se houver uma seção alvo, rola até ela
+  if (targetSection) {
+    e.preventDefault();
+    scrollDown(targetSection.id, 2000);
+  }
+}
+
+// Adiciona o event listener para o evento de 'wheel'
+document.addEventListener('wheel', handleWheelEvent, { passive: false });
+
   
