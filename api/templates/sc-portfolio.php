105aad6803a9f3c2118f248a33145546d584c95a
BrandsDev <lucasbrandao392@gmail.com>
2024-04-15 22:32:37 -0300

    Testando sc-prog

--- api/templates/sc-portfolio.php
+++ api/templates/sc-portfolio.php
@@ -10,13 +10,13 @@
 
                     <div class="col-lg-7">
                         <div class="row gy-4">
-                            <a href="<?= $url; ?>/img/project_01.png" class="c-card-project rounded-5 col-md-6" data-toggle="lightbox"  data-gallery="gallery-portfolio" data-caption="Project 01">
+                            <a href="<?= $url; ?>/img/project_01.png" class="c-card-project rounded-5 col-md-6" data-toggle="lightbox" data-gallery="gallery-portfolio">
                                 <img src="<?= $url; ?>/img/project_01.png" alt="project_01" class="c-card-project__image">
                             </a>
-                            <a href="<?= $url; ?>/img/project_02.png" class="c-card-project rounded-5 col-md-6" data-toggle="lightbox"  data-gallery="gallery-portfolio" data-caption="Project 02">
+                            <a href="<?= $url; ?>/img/project_02.png" class="c-card-project rounded-5 col-md-6" data-toggle="lightbox" data-gallery="gallery-portfolio">
                                 <img src="<?= $url; ?>/img/project_02.png" alt="project_02" class="c-card-project__image">
                             </a>
-                            <a href="<?= $url; ?>/img/project_03.png" class="c-card-project rounded-5 col-12" data-toggle="lightbox"  data-gallery="gallery-portfolio" data-caption="Project 03">
+                            <a href="<?= $url; ?>/img/project_03.png" class="c-card-project rounded-5 col-12" data-toggle="lightbox"  data-gallery="gallery-portfolio">
                                 <img src="<?= $url; ?>/img/project_03.png" alt="project_03" class="c-card-project__image">
                             </a>
                         </div>
--- api/templates/sc-programas.php
+++ api/templates/sc-programas.php
@@ -8,28 +8,28 @@
                     <h1 class="fw-light mb-0">Minhas criações se forma <br> <span class="h1 text-primary fw-light">em muitas telas! </span> </h1>
                     <p class="opacity-50 fw-lighter mb-5">Essas são as minhas vantagens</p>
                     <div class="circle">
-                        <div class="prog"> 
-                            <span class="react"> React </span>
+                        <div class="prog react"> 
+                            <span> React </span>
                             <img src="<?= $url; ?>/img/icon-react.png" width="40" alt="react">
                         </div>
-                        <div class="prog"> 
-                            <span class="framer"> Framer </span>
+                        <div class="prog framer"> 
+                            <span> Framer </span>
                             <img src="<?= $url; ?>/img/icon-react.png" width="40" alt="framer">
                         </div>
-                        <div class="prog"> 
-                            <span class="webflow"> WebFlow </span>
+                        <div class="prog webflow"> 
+                            <span> WebFlow </span>
                             <img src="<?= $url; ?>/img/icon-react.png" width="40" alt="webflow">
                         </div>
-                        <div class="prog"> 
-                            <span class="figma"> Figma </span>
+                        <div class="prog figma"> 
+                            <span> Figma </span>
                             <img src="<?= $url; ?>/img/icon-react.png" width="40" alt="figma">
                         </div>
-                        <div class="prog"> 
-                            <span class="wordpress"> WordPress </span>
+                        <div class="prog wordpress"> 
+                            <span> WordPress </span>
                             <img src="<?= $url; ?>/img/icon-react.png" width="40" alt="wordpress">
                         </div>
-                        <div class="prog"> 
-                            <span class="laravel"> Laravel/PHP </span>
+                        <div class="prog laravel"> 
+                            <span> Laravel/PHP </span>
                             <img src="<?= $url; ?>/img/icon-react.png" width="40" alt="laravel">
                         </div>
                     </div>
