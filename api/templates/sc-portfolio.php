800f4089dd6add5e63194887f52ed880896a9aa5
BrandsDev <lucasbrandao392@gmail.com>
2024-04-15 22:38:50 -0300

    Testando sc-prog e port

--- api/templates/sc-portfolio.php
+++ api/templates/sc-portfolio.php
@@ -10,14 +10,25 @@
 
                     <div class="col-lg-7">
                         <div class="row gy-4">
-                            <a href="<?= $url; ?>/img/project_01.png" class="c-card-project rounded-5 col-md-6" data-toggle="lightbox" data-gallery="gallery-portfolio">
+                            <a href="<?= $url; ?>/img/project_01.png" data-toggle="lightbox" data-gallery="example-gallery" class="c-card-project rounded-5 col-md-6">
                                 <img src="<?= $url; ?>/img/project_01.png" alt="project_01" class="c-card-project__image">
                             </a>
-                            <a href="<?= $url; ?>/img/project_02.png" class="c-card-project rounded-5 col-md-6" data-toggle="lightbox" data-gallery="gallery-portfolio">
+                            <a href="<?= $url; ?>/img/project_02.png" data-toggle="lightbox" data-gallery="example-gallery" class="c-card-project rounded-5 col-md-6">
                                 <img src="<?= $url; ?>/img/project_02.png" alt="project_02" class="c-card-project__image">
                             </a>
-                            <a href="<?= $url; ?>/img/project_03.png" class="c-card-project rounded-5 col-12" data-toggle="lightbox"  data-gallery="gallery-portfolio">
+                            <a href="<?= $url; ?>/img/project_03.png" data-toggle="lightbox" data-gallery="example-gallery" class="c-card-project rounded-5 col-12">
                                 <img src="<?= $url; ?>/img/project_03.png" alt="project_03" class="c-card-project__image">
+                            </a>
+                        </div>
+                        <div class="row d-none">
+                            <a href="https://unsplash.it/1200/768.jpg?image=251" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-4">
+                                <img src="https://unsplash.it/600.jpg?image=251" class="img-fluid">
+                            </a>
+                            <a href="https://unsplash.it/1200/768.jpg?image=252" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-4">
+                                <img src="https://unsplash.it/600.jpg?image=252" class="img-fluid">
+                            </a>
+                            <a href="https://unsplash.it/1200/768.jpg?image=253" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-4">
+                                <img src="https://unsplash.it/600.jpg?image=253" class="img-fluid">
                             </a>
                         </div>
                     </div>
