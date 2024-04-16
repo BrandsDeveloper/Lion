d48901f2aab9512a39103ae9aec1bef7d9425f95
BrandsDev <lucasbrandao392@gmail.com>
2024-04-15 21:12:09 -0300

    Inserindo serviços

--- api/templates/sc-servicos.php
+++ api/templates/sc-servicos.php
@@ -5,7 +5,23 @@
             <div class="col-lg-8">
                 <div class="d-flex flex-column h-100 align-items-start justify-content-center ">
                     <p class="py-1 px-4 rounded-5 border border-1 border-primary text-primary mb-5"> Serviços </p>
-                    
+                    <div class="cards-servicos">
+                        <div class="rounded-4 border border-light border-2 p-3">
+                            <h2 class="fw-light">Design d/ Web Site</h2>
+                            <p>Idealizo e desenvolvo estéticas únicas para produtos e marcas digitais usando Figma e Frame.</p>
+                            <button class="text-primary"> + Meus projetos</button>
+                        </div>
+                        <div class="ms-5 rounded-4 border border-light border-2 p-3">
+                            <h2 class="fw-light">Desenvolvedor [dev]</h2>
+                            <p>Constuo Layouts únicos para site que vão ao ar com Framer Webflow ou WordPress.</p>
+                            <button class="text-primary"> + Meus projetos</button>
+                        </div>
+                        <div class="rounded-4 border border-light border-2 p-3">
+                            <h2 class="fw-light">SEO/Marketing</h2>
+                            <p>Aumentando o tráfego do seu website com otimização de SEO.</p>
+                            <button class="text-primary"> + Meus projetos</button>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-auto opacity-50">
