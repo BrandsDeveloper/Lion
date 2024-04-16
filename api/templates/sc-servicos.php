a27c0feed28c787845ff62dad33a78534dbd4173
BrandsDev <lucasbrandao392@gmail.com>
2024-04-15 21:28:07 -0300

    Ajustando servicos sass

--- api/templates/sc-servicos.php
+++ api/templates/sc-servicos.php
@@ -6,20 +6,20 @@
                 <div class="d-flex flex-column h-100 align-items-start justify-content-center ">
                     <p class="py-1 px-4 rounded-5 border border-1 border-primary text-primary mb-5"> Serviços </p>
                     <div class="cards-servicos">
-                        <div class="rounded-50 border border-light border-2 p-4">
+                        <div class="rounded-50 border border-light border-2 p-4 pe-5">
                             <h2 class="fw-light">Design d/ Web Site</h2>
                             <p class="small opacity-50">Idealizo e desenvolvo estéticas únicas para produtos e marcas digitais usando Figma e Frame.</p>
-                            <button class="text-primary btn p-0"> + Meus projetos</button>
+                            <button class="btn fw-lighter opacity-50 p-0 text-primary"> + Meus projetos</button>
                         </div>
-                        <div class="my-4 ms-5 rounded-50 border border-light border-2 p-4">
+                        <div class="my-4 ms-5 rounded-50 border border-light border-2 p-4 pe-5">
                             <h2 class="fw-light">Desenvolvedor [dev]</h2>
                             <p class="small opacity-50">Constuo Layouts únicos para site que vão ao ar com Framer Webflow ou WordPress.</p>
-                            <button class="text-primary btn p-0"> + Meus projetos</button>
+                            <button class="btn fw-lighter opacity-50 p-0 text-primary"> + Meus projetos</button>
                         </div>
-                        <div class="rounded-50 border border-light border-2 p-4">
+                        <div class="rounded-50 border border-light border-2 p-4 pe-5">
                             <h2 class="fw-light">SEO/Marketing</h2>
                             <p class="small opacity-50">Aumentando o tráfego do seu website com otimização de SEO.</p>
-                            <button class="text-primary btn p-0"> + Meus projetos</button>
+                            <button class="btn fw-lighter opacity-50 p-0 text-primary"> + Meus projetos</button>
                         </div>
                     </div>
                 </div>
