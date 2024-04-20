2724e8ea4ffd82586e99df04bb37cbbe6c200f24
BrandsDev <lucasbrandao392@gmail.com>
2024-04-19 22:07:18 -0300

    Ajuste feedback

--- api/templates/sc-feedback.php
+++ api/templates/sc-feedback.php
@@ -7,62 +7,25 @@
                     <p class="py-1 px-4 rounded-5 border border-1 border-primary text-primary mb-5"> Feedback </p>
                     <h1 class="fw-light mb-0">Além das <span class="h1 text-primary fw-light">telas! </span> </h1>
                     <p class="opacity-50 fw-lighter">Cada experiência se torna única a cada arte</p>
-
-                    <!-- Demo styles -->
-                    <style>
-                        html,
-                        body {
-                        position: relative;
-                        height: 100%;
-                        }
-
-                        body {
-                        background: #eee;
-                        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-                        font-size: 14px;
-                        color: #000;
-                        margin: 0;
-                        padding: 0;
-                        }
-
-                        .swiper {
-                        width: 100%;
-                        height: 100%;
-                        }
-
-                        .swiper-slide {
-                        text-align: center;
-                        font-size: 18px;
-                        background: #fff;
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        }
-
-                        .swiper-slide img {
-                        display: block;
-                        width: 100%;
-                        height: 100%;
-                        object-fit: cover;
-                        }
-                    </style>
                     
-                    <div class="swiper testemunho">
+                    <div class="swiper depoimento">
                         <div class="swiper-wrapper">
+                            <?php for ($i=0; $i < 3; $i++) : ?>
                             <div class="swiper-slide">
-                                <div class="p-4 rounded-5 border border-light">
+                                <div class="p-4 rounded-5 border border-light c-card-depoimento">
                                     <div class="row">
-                                        <div class="col-auto"> <img src="" class="rounded-5" alt=""> </div>
+                                        <div class="col-auto"> <img src="<?= $url; ?>/img/image-sem-filtro.png" class="c-card-depoimento__image" alt=""> </div>
                                         <div class="col">
-                                            <p class="fs-3 text-white">Victor Bronzer</p>
-                                            <p class="ps-4"> Diretor criativo de <span class="text-primary">Paraglider Game</span> </p>
+                                            <h3 class="text-white m-0">Victor Bronzer</h3>
+                                            <p class="ps-4 small"> Diretor criativo de <span class="text-primary">Paraglider Game</span> </p>
                                         </div>
                                     </div>
                                     <div>
-                                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum itaque perferendis explicabo saepe ratione culpa facilis, asperiores suscipit voluptate unde molestiae soluta rerum vitae praesentium odit, dignissimos accusantium sequi fugit? </p>
+                                        <p class="small opacity-50"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum itaque perferendis explicabo saepe ratione culpa facilis, asperiores suscipit voluptate unde molestiae soluta rerum vitae praesentium odit, dignissimos accusantium sequi fugit? </p>
                                     </div>
                                 </div>
                             </div>
+                            <?php endfor; ?>
                         </div>
                         <div class="swiper-button-next"></div>
                         <div class="swiper-button-prev"></div>
@@ -79,7 +42,7 @@
 </section>
 
 <script>
-    var swiper = new Swiper(".testemunho", {
+    var swiper = new Swiper(".depoimento", {
         pagination: {
         el: ".swiper-pagination",
         type: "fraction",
