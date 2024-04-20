5198e3f49f46c277c96c7cd7c181b09d6613ae97
BrandsDev <lucasbrandao392@gmail.com>
2024-04-19 23:08:33 -0300

    Ajuste feedback 6

--- api/templates/sc-feedback.php
+++ api/templates/sc-feedback.php
@@ -12,17 +12,23 @@
                         <div class="swiper depoimento">
                             <div class="swiper-wrapper">
                                 <?php for ($i=0; $i < 3; $i++) : ?>
-                                <div class="swiper-slide">
+                                <div class="swiper-slide pe-5">
                                     <div class="p-4 rounded-5 border border-light c-card-depoimento">
-                                        <div class="row">
+                                        <div class="row mb-4">
                                             <div class="col-auto"> <img src="<?= $url; ?>/img/image-sem-filtro.png" class="c-card-depoimento__image" alt=""> </div>
                                             <div class="col">
                                                 <h3 class="text-white m-0">Victor Bronzer</h3>
-                                                <p class="ps-4 small"> Diretor criativo de <span class="text-primary">Paraglider Game</span> </p>
+                                                <p class="ps-4 small c-card-depoimento__bar"> Diretor criativo de <span class="text-primary">Paraglider Game</span> </p>
                                             </div>
                                         </div>
                                         <div>
                                             <p class="small opacity-50"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum itaque perferendis explicabo saepe ratione culpa facilis, asperiores suscipit voluptate unde molestiae soluta rerum vitae praesentium odit, dignissimos accusantium sequi fugit? </p>
+                                        </div>
+                                        <div class="d-flex justify-content-between">
+                                            <p class="small"> Veja o <span class="text-primary">projeto de Victor!</span> </p>
+                                            <div>
+                                                <p class="small opacity-50 m-0">Incrível</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
