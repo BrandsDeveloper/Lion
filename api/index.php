5f0d0e442d27372747f62edac93f1ba6a74bf95d
BrandsDev <lucasbrandao392@gmail.com>
2024-04-21 13:21:25 -0300

    Testar conexão com o banco

--- api/index.php
+++ api/index.php
@@ -5,6 +5,7 @@
  * @source project-lion
  * 
 **/
+phpinfo();
 
 include ('header.php');
 
@@ -18,6 +19,31 @@ include ('templates/sc-portfolio.php');
 include ('templates/sc-feedback.php');
 include ('templates/sc-contato.php');
 echo '</main>';
+
+
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_TABLE'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+
+// $conn = "pgsql:port=$port;dbname=$dbname;user=$user;password=$password;host=$host;";
+
+try {
+    // Criar uma conexão PDO
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
+
+    if($pdo){
+        // echo "Conectado ao banco de dados PostgreSQL com sucesso!";
+        echo "Conectado ao banco de dados MYSQL com sucesso!";
+    }
+
+} catch (PDOException $e) {
+    // Se ocorrer um erro na conexão, ele será exibido aqui
+    echo 'Encontramos um erro na conexão!! <br>';
+    echo $e->getMessage();
+}
 
 include ('footer.php'); 
 
