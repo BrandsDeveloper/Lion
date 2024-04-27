9fe290f5495a2f2f8b432199193d8889fe958520
BrandsDev <lucasbrandao392@gmail.com>
2024-04-27 16:33:59 -0300

    Settings three

--- conn.php
+++ conn.php
@@ -1,23 +1,25 @@
 <?php
 
+phpinfo();
 var_dump($GITHUB_ENV);
+
 $host = $_ENV['DB_HOST'];
 $port = $_ENV['DB_PORT'];
 $dbname = $_ENV['DB_TABLE'];
 $user = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASS'];
 
-try {
+// try {
 
-    // Criar uma conexão PDO
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
+//     // Criar uma conexão PDO
+//     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
     
-    if($pdo){
-        echo "Conectado ao banco de dados MYSQL com sucesso! ";
-    }
+//     if($pdo){
+//         echo "Conectado ao banco de dados MYSQL com sucesso! ";
+//     }
 
-} catch (PDOException $e) {
-    // Se ocorrer um erro na conexão, ele será exibido aqui
-    echo 'Encontramos um erro na conexão!! <br>';
-    echo $e->getMessage();
-}
+// } catch (PDOException $e) {
+//     // Se ocorrer um erro na conexão, ele será exibido aqui
+//     echo 'Encontramos um erro na conexão!! <br>';
+//     echo $e->getMessage();
+// }
