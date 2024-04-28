32e761a652b2600b1af24fa6c5dbe4f05603c1c0
BrandsDev <lucasbrandao392@gmail.com>
2024-04-28 12:41:19 -0300

    Up

--- conn.php
+++ conn.php
@@ -1,13 +1,25 @@
 <?php
 
-phpinfo();
-var_dump($GITHUB_ENV);
+$dbHost = $_SERVER['DB_HOST'];
+$dbTable = $_SERVER['DB_TABLE'];
+$dbUser = $_SERVER['DB_USER'];
+$dbPass = $_SERVER['DB_PASS'];
 
-$host = $_ENV['DB_HOST'];
-$port = $_ENV['DB_PORT'];
-$dbname = $_ENV['DB_TABLE'];
-$user = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASS'];
+// Use as variáveis conforme necessário no seu projeto
+echo "Host do banco de dados: $dbHost<br>";
+echo "Tabela do banco de dados: $dbTable<br>";
+echo "Usuário do banco de dados: $dbUser<br>";
+echo "Senha do banco de dados: $dbPass<br>";
+
+phpinfo();
+
+// var_dump($GITHUB_ENV);
+
+// $host = $_ENV['DB_HOST'];
+// $port = $_ENV['DB_PORT'];
+// $dbname = $_ENV['DB_TABLE'];
+// $user = $_ENV['DB_USER'];
+// $password = $_ENV['DB_PASS'];
 
 // try {
 
