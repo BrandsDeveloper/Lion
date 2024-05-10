9815c9a5e5fbe55fe72b718000be9c7d1e075916
BrandsDev <lucasbrandao392@gmail.com>
2024-05-09 23:08:58 -0300

    modal forms

---
+++ templates/modal.php
@@ -0,0 +1,19 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary d-none" id="modal-ajax" data-bs-toggle="modal" data-bs-target="#modalAjax">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="modalAjax" tabindex="-1" aria-labelledby="modalAjaxLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-body">
+          <div class="d-flex justify-content-between">
+              <h1 class="modal-title fs-4" id="ajax-title">Modal title</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <p id="ajax-content" class="pt-4"></p>
+      </div>
+    </div>
+  </div>
+</div>
